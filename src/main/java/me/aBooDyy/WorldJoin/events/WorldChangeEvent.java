5664cbf9af6caee8c2f015f18f4f602be2dcf623
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2019-04-30 21:04:01 +0300

    changed IFs startsWith with switch using regex

--- src/main/java/me/aBooDyy/WorldJoin/events/WorldChangeEvent.java
+++ src/main/java/me/aBooDyy/WorldJoin/events/WorldChangeEvent.java
@@ -8,9 +8,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static me.aBooDyy.WorldJoin.actions.ConsoleAction.consoleAction;
-import static me.aBooDyy.WorldJoin.actions.MessageAction.messageAction;
+import static me.aBooDyy.WorldJoin.actions.MessageAction.*;
 import static me.aBooDyy.WorldJoin.actions.PlayerAction.playerAction;
 
 public class WorldChangeEvent implements Listener {
@@ -26,17 +28,22 @@ public class WorldChangeEvent implements Listener {
         World from = e.getFrom().getWorld();
         World to = e.getTo().getWorld();
 
-
         if (pl.getConfig().get("worlds." + to.getName()) != null) {
             if (e.getFrom().getWorld() != e.getTo().getWorld()) {
                 List<String> actions = pl.getConfig().getStringList("worlds." + to.getName() + ".actions");
                 for (String action : actions) {
-                    if (action.toLowerCase().startsWith("[console] ")) {
-                        consoleAction(p, from, to, action);
-                    } else if (action.toLowerCase().startsWith("[player] ")) {
-                        playerAction(p, from, to, action);
-                    } else if (action.toLowerCase().startsWith("[message] ")) {
-                        messageAction(p, from, to, action);
+                    Matcher actionType = Pattern.compile("\\[([^)]+)]").matcher(action);
+                    String type = actionType.find() ? actionType.group(1).toLowerCase() : "";
+                    switch (type) {
+                        case "console":
+                            consoleAction(p, from, to, action);
+                            break;
+                        case "player":
+                            playerAction(p, from, to, action);
+                            break;
+                        case "message":
+                            messageAction(p, from, to, action);
+                            break;
                     }
                 }
             }
--- src/main/java/me/aBooDyy/WorldJoin/events/WorldJoinEvent.java
+++ src/main/java/me/aBooDyy/WorldJoin/events/WorldJoinEvent.java
@@ -8,6 +8,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static me.aBooDyy.WorldJoin.actions.ConsoleAction.consoleAction;
 import static me.aBooDyy.WorldJoin.actions.MessageAction.messageAction;
@@ -24,16 +26,24 @@ public class WorldJoinEvent implements Listener {
     public void joinWorld(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         World world = p.getLocation().getWorld();
+
         if (pl.getConfig().get("worlds." + world.getName()) != null) {
             if (pl.getConfig().getBoolean("worlds." + world.getName() + ".run_on_join")) {
                 List<String> actions = pl.getConfig().getStringList("worlds." + world.getName() + ".actions");
                 for (String action : actions) {
-                    if (action.toLowerCase().startsWith("[console] ")) {
-                        consoleAction(p, world, world, action);
-                    } else if (action.toLowerCase().startsWith("[player] ")) {
-                        playerAction(p, world, world, action);
-                    } else if (action.toLowerCase().startsWith("[message] ")) {
-                        messageAction(p, world, world, action);
+                    Matcher actionType = Pattern.compile("\\[([^)]+)]").matcher(action);
+                    String type = actionType.find() ? actionType.group(1).toLowerCase() : "";
+
+                    switch (type) {
+                        case "console":
+                            consoleAction(p, world, world, action);
+                            break;
+                        case "player":
+                            playerAction(p, world, world, action);
+                            break;
+                        case "message":
+                            messageAction(p, world, world, action);
+                            break;
                     }
                 }
             }
