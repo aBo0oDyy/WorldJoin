1d2d7649e56e9da7091bc4026613b0fca646f9ec
aBo0oDyy <35378106+aBo0oDyy@users.noreply.github.com>
2020-01-06 02:20:36 +0300

    Better help menu! JSON :DD

--- src/main/java/net/aboodyy/worldjoin/Utils.java
+++ src/main/java/net/aboodyy/worldjoin/Utils.java
@@ -1,7 +1,10 @@
 package net.aboodyy.worldjoin;
 
+import net.md_5.bungee.api.chat.*;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
+
+import static net.aboodyy.worldjoin.WorldJoin.plugin;
 
 public class Utils {
     public static String color(String text) {
@@ -13,5 +16,35 @@ public class Utils {
                 .replaceAll("(?i)" + "\\{world_from}", from.getName())
                 .replaceAll("(?i)" + "\\{world_to}", to.getName());
         return text;
+    }
+
+    public static TextComponent getMessage(String display, String suggestion, String tooltip) {
+        TextComponent message = new TextComponent(color(display));
+
+        BaseComponent[] tt = new ComponentBuilder(color(tooltip)).create();
+        message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tt));
+
+        ClickEvent click = new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, suggestion);
+        message.setClickEvent(click);
+
+        return message;
+    }
+
+    public static TextComponent getHeader() {
+        TextComponent right = new TextComponent(color("&7&m                 "));
+        TextComponent left = new TextComponent(color("&7&m                 "));
+        TextComponent im = new TextComponent(color("&2 WorldJoin "));
+        BaseComponent[] tooltip = new ComponentBuilder(
+                color("\n &2Version: &f" + plugin.getDescription().getVersion() + " \n &2Author: &faBooDyy \n" +
+                        " &2Contact: &faBooDyy.net/Discord \n &2Command aliases: &f/WorldJoin, /WJoin \n"))
+                .create();
+
+        im.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tooltip));
+        im.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org/resources/63892/"));
+
+        right.addExtra(im);
+        right.addExtra(left);
+
+        return right;
     }
 }
--- src/main/java/net/aboodyy/worldjoin/commands/WorldJoinCommand.java
+++ src/main/java/net/aboodyy/worldjoin/commands/WorldJoinCommand.java
@@ -3,8 +3,9 @@ package net.aboodyy.worldjoin.commands;
 import me.mattstudios.mf.annotations.*;
 import me.mattstudios.mf.base.CommandBase;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-import static net.aboodyy.worldjoin.Utils.color;
+import static net.aboodyy.worldjoin.Utils.*;
 import static net.aboodyy.worldjoin.WorldJoin.plugin;
 
 @Command("worldjoin")
@@ -20,12 +21,72 @@ public class WorldJoinCommand extends CommandBase {
     @SubCommand("help")
     @Permission("worldjoin.help")
     public void onHelp(CommandSender sender) {
-        sender.sendMessage(color("&aWorld&7Join &fv" + plugin.getDescription().getVersion()));
-        sender.sendMessage(color("&a/WorldJoin help &f- &7About the plugin"));
-        sender.sendMessage(color("&a/WorldJoin reload &f- &7To reload the config file"));
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+
+            p.spigot().sendMessage(getHeader());
+            p.sendMessage("");
+
+            p.spigot().sendMessage(getMessage(" &a/WorldJoin Help",
+                    "/worldjoin help",
+                    "\n &fDisplays all the plugin's command. \n\n" +
+                            " &2Usage: &f/WJoin Help \n" +
+                            " &2Permission: &fWorldJoin.Help \n"
+            ));
+
+            p.spigot().sendMessage(getMessage(" &a/WorldJoin Reload",
+                    "/worldjoin reload",
+                    "\n &fReloads the plugin's config file. \n\n" +
+                            " &2Usage: &f/WJoin Reload \n" +
+                            " &2Permission: &fWorldJoin.Reload \n"
+            ));
+            p.sendMessage("");
+
+            p.spigot().sendMessage(getMessage(" &a/WorldJoin SpecialList",
+                    "/worldjoin speciallist",
+                    "\n &fDisplays the current special actions\n for the specified player in the specified world. \n\n" +
+                            " &2Usage: &f/WJoin SpecialList <Player> <World> \n" +
+                            " &2Permission: &fWorldJoin.Special.List \n"
+            ));
+
+            p.spigot().sendMessage(getMessage(" &a/WorldJoin AddSpecial",
+                    "/worldjoin addspecial",
+                    "\n &fAdds a special action. \n\n" +
+                            " &2Usage: &f/WJoin AddSpecial <Player> <World> <ActionType> <Action> \n" +
+                            " &2Permission: &fWorldJoin.Special.Add \n"
+            ));
+
+            p.spigot().sendMessage(getMessage(" &a/WorldJoin RemoveSpecial",
+                    "/worldjoin removespecial",
+                    "\n &fRemoves a special action. \n\n" +
+                            " &2Usage: &f/WJoin RemoveSpecial <Player> <World> <Index> \n" +
+                            " &2Permission: &fWorldJoin.Special.Remove \n"
+            ));
+
+            p.spigot().sendMessage(getMessage(" &a/WorldJoin ClearSpecial",
+                    "/worldjoin clearspecial",
+                    "\n &fClears/Removes all the special actions. \n\n" +
+                            " &2Usage: &f/WJoin ClearSpecial <Player> <World> \n" +
+                            " &2Permission: &fWorldJoin.Special.Clear \n"
+            ));
+            p.sendMessage("");
+
+            p.sendMessage(color("&7&m                                                  "));
+            return;
+        }
+
+        sender.sendMessage("");
+        sender.sendMessage(color("&2WorldJoin &fv" + plugin.getDescription().getVersion()));
+        sender.sendMessage("");
+        sender.sendMessage(color("&a/WorldJoin Help"));
+        sender.sendMessage(color("&a/WorldJoin Reload"));
         sender.sendMessage(" ");
-        sender.sendMessage(color("&aSupport: &7https://aBooDyy.net/discord"));
-        sender.sendMessage(color("&aby: &7aBooDyy"));
+        sender.sendMessage(color("&a/WorldJoin SpecialList"));
+        sender.sendMessage(color("&a/WorldJoin AddSpecial"));
+        sender.sendMessage(color("&a/WorldJoin RemoveSpecial"));
+        sender.sendMessage(color("&a/WorldJoin ClearSpecial"));
+        sender.sendMessage("");
     }
 
 }
